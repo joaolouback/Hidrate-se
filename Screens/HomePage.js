9277459469fa954e19b60b644f9edd87541a4961
joaolouback <jpestevaoloubackbr@gmail.com>
2024-10-30 10:05:33 -0300

    Salvando progresso antes da ejeção do Expo

--- Screens/HomePage.js
+++ Screens/HomePage.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Alert, ActivityIndicator } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
-export default function RouteScreen() {
+export default function HomePage() {
+  const [region, setRegion] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      // Solicita permissão de localização
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permissão de localização negada', 'Permita o acesso à localização para usar o mapa');
+        setLoading(false);
+        return;
+      }
+
+      // Obtém a localização atual
+      let location = await Location.getCurrentPositionAsync({});
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      });
+      setLoading(false); // Define que o carregamento terminou
+    })();
+  }, []);
+
+  // Exibe um indicador de carregamento enquanto a localização está sendo obtida
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo à página Route!</Text>
+      <MapView
+        style={styles.map}
+        region={region}
+        onRegionChangeComplete={(region) => setRegion(region)}
+        showsUserLocation={true}
+      >
+        <Marker
+          coordinate={{ latitude: -20.342207, longitude: -40.292783 }}
+          title="Ponto de Água"
+          description="Av. Luciano das Neves, 2418 - Centro de Vila Velha"
+        />
+      </MapView>
     </View>
   );
 }
@@ -12,13 +59,14 @@ export default function RouteScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#0F51A4',
   },
-  text: {
-    fontSize: 24,
-    color: '#fff',
-    fontWeight: 'bold',
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
